bb625b146455589e14a610852e1a50a1aa654db9
Evette Telyas <etelyas@gmail.com>
2019-09-12 16:03:12 -0600

    update tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Course, type: :model do
     describe "validations" do
         it { should validate_presence_of :name }
     end
+
+    describe "model methods" do
+        before :each do
+            @pugs_101 = Course.create(name: "Pugs 101")
+            @larry = Student.create(name: "Larry")
+            @evette = Student.create(name: "Evette")
+            StudentCourse.create(student: @larry, course: @pugs_101, grade: 100)
+            StudentCourse.create(student: @evette, course: @pugs_101, grade: 99.98)
+        end
+
+        it "should find student grade" do
+            expect(@pugs_101.find_student_grade(@evette)).to eq(99.98)
+        end
+
+        it "should sort students by grade" do
+            expect(@pugs_101.sort_students_by_grade).to eq([@larry, @evette])
+        end
+    end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Student, type: :model do
     describe "valiations" do
         it { should validate_presence_of :name }
     end
+
+    describe "model methods" do
+        before :each do
+            @pugs_101 = Course.create(name: "Pugs 101")
+            @larry = Student.create(name: "Larry")
+            @evette = Student.create(name: "Evette")
+            StudentCourse.create(student: @larry, course: @pugs_101, grade: 100)
+            StudentCourse.create(student: @evette, course: @pugs_101, grade: 99.98)        
+        end
+
+        it "should find grade for a course" do
+            expect(@evette.find_grade(@pugs_101)).to eq(99.98)
+        end
+    end
 end
